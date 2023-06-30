6824ddfd9287b6b719068caea79dcc15e11d0dc8
katiemcmillin <katiemcmillin@gmail.com>
2023-06-29 21:15:24 -0400

    fixed test 4

--- spotify_project/settings.py
+++ spotify_project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'spotify_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         
-        default='postgrs://katiemcmillin:5432@localhost/spotify', conn_max_age=600, conn_health_checks=True
+        default='postgres://katiemcmillin:5432@localhost/spotify', conn_max_age=600, conn_health_checks=True
     )
 }
 
